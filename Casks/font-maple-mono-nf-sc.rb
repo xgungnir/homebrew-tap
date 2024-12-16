a71be1458f302ff21814635263151fcfc9d556fa
xgungnir <6522340+xgungnir@users.noreply.github.com>
2024-12-16 14:16:50 +0800

    Add font-maple-mono-nf-sc 7.0-beta32

---
+++ Casks/font-maple-mono-nf-sc.rb
@@ -0,0 +1,25 @@
+cask "font-maple-mono-nf-sc" do
+  version "7.0-beta32"
+
+  url "https://github.com/subframe7536/maple-font/releases/download/v#{version}/MapleMono-NF-CN-unhinted.zip"
+  name "Maple Mono NF SC"
+  homepage "https://github.com/subframe7536/Maple-font"
+
+  font "MapleMono-NF-CN-Bold.ttf"
+  font "MapleMono-NF-CN-BoldItalic.ttf"
+  font "MapleMono-NF-CN-ExtraBold.ttf"
+  font "MapleMono-NF-CN-ExtraBoldItalic.ttf"
+  font "MapleMono-NF-CN-ExtraLight.ttf"
+  font "MapleMono-NF-CN-ExtraLightItalic.ttf"
+  font "MapleMono-NF-CN-Italic.ttf"
+  font "MapleMono-NF-CN-Light.ttf"
+  font "MapleMono-NF-CN-LightItalic.ttf"
+  font "MapleMono-NF-CN-Medium.ttf"
+  font "MapleMono-NF-CN-MediumItalic.ttf"
+  font "MapleMono-NF-CN-Regular.ttf"
+  font "MapleMono-NF-CN-SemiBold.ttf"
+  font "MapleMono-NF-CN-SemiBoldItalic.ttf"
+  font "MapleMono-NF-CN-Thin.ttf"
+  font "MapleMono-NF-CN-ThinItalic.ttf"
+
+end
