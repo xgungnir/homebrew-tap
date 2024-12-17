734bf4bc1e37daf28dc81df0dc66839add11c295
xgungnir <6522340+xgungnir@users.noreply.github.com>
2024-12-17 10:48:48 +0800

    Add siyuan-ce 3.1.15

---
+++ Casks/siyuan-ce.rb
@@ -0,0 +1,19 @@
+cask "siyuan-ce" do
+  arch arm: "-arm64"
+
+  version "3.1.15"
+
+  url "https://github.com/siyuan-community/siyuan/releases/download/v#{version}/siyuan-CE-v#{version}-mac#{arch}.dmg"
+  name "SiYuan CE"
+  desc "Local-first personal knowledge management system"
+  homepage "https://github.com/siyuan-community/siyuan"
+
+  app "SiYuan.app"
+
+  zap trash: [
+    "~/.config/.siyuan",
+    "~/Library/Application Support/SiYuan-Electron",
+    "~/Library/Preferences/org.b3log.siyuan.plist",
+    "~/Library/Saved Application State/org.b3log.siyuan.savedState",
+  ]
+end
