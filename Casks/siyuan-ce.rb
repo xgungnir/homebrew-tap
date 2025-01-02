0d4ed9a42d2da5617a3674b394e84c9006472785
xgungnir <6522340+xgungnir@users.noreply.github.com>
2025-01-02 09:28:11 +0800

    clashx-meta 1.4.7 and siyuan-cd 3.1.17

--- Casks/siyuan-ce.rb
+++ Casks/siyuan-ce.rb
@@ -1,7 +1,7 @@
 cask "siyuan-ce" do
   arch arm: "-arm64"
 
-  version "3.1.15"
+  version "3.1.17"
 
   url "https://github.com/siyuan-community/siyuan/releases/download/v#{version}/siyuan-CE-v#{version}-mac#{arch}.dmg"
   name "SiYuan CE"
